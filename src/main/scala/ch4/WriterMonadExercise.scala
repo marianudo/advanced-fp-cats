512ee047b9d4f49378b1ac2e5892621715c382c7
Mariano Navas <marianudo@gmail.com>
2016-09-12 12:43:49 +0200

    Solve factorial with writer monad
    To isolate logs for each concurrent process.

--- src/main/scala/ch4/WriterMonadExercise.scala
+++ src/main/scala/ch4/WriterMonadExercise.scala
@@ -10,18 +10,31 @@ object WriterMonadExercise extends App {
 
     import scala.concurrent.duration._
 
-    def slowly[A](body: => A) =
+    import cats.data.Writer
+    import cats.instances.vector._
+    import cats.syntax.writer._
+    import cats.syntax.applicative._
+
+    type Logged[A] = Writer[Vector[String], A]
+
+    def slowly[A](body: => A): A =
         try body finally Thread.sleep(100)
 
-    def factorial(n: Int): Int = {
-        val ans = slowly(if (n == 0) 1 else n * factorial(n - 1))
-        println(s"fact $n $ans")
-        ans
+    def factorial(n: Int): Logged[Int] = {
+        if(n == 0) 1.pure[Logged]
+        else for {
+            a <- slowly(factorial(n - 1))
+            _ <- Vector(s"fact $n ${a * n}").tell
+        } yield a * n
     }
 
-    Await.result(
+    val result = Await.result(
         Future.sequence(
-            Vector(Future(factorial(5)), Future(factorial(5)))
+            Vector(Future(factorial(5).run), Future(factorial(5).run))
         ), Duration.Inf
     )
+
+//    println(factorial(5).run)
+
+    result.foreach(println)
 }
