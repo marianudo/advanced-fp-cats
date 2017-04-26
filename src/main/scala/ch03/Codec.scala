ebd5651a0bdb86b87d3215c98c4931ffe67e4c68
Mariano Navas <mariano.navas@clluc.com>
2017-04-26 13:28:56 +0200

    Add Codec exercise for chapter 3
    Just the "planteamiento".

---
+++ src/main/scala/ch03/Codec.scala
@@ -0,0 +1,17 @@
+package ch03
+
+trait Codec[A] {
+  def encode(value: A): String
+  def decode(value: String): Option[A]
+  
+  def imap[B](dec: A => B, enc: B => A): Codec[B] =
+    ???
+}
+
+object Codec {
+  def encode[A](value: A)(implicit c: Codec[A]): String =
+    c.encode(value)
+
+  def decode[A](value: String)(implicit c: Codec[A]): Option[A] =
+    c.decode(value)
+}
