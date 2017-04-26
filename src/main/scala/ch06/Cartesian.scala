ec42d108eb2d44f2a2e1c52e826a5bdc5b696b57
Mariano Navas <mariano.navas@clluc.com>
2017-04-26 18:54:08 +0200

    Implement an OptionCartesian
    Just as an exercise.

--- src/main/scala/ch06/Cartesian.scala
+++ src/main/scala/ch06/Cartesian.scala
@@ -5,3 +5,14 @@ import scala.language.higherKinds
 trait Cartesian[F[_]] {
   def product[A, B](fa: F[A], fb: F[B]): F[(A, B)]
 }
+
+object Cartesian {
+
+  implicit object OptionCartesian extends Cartesian[Option] {
+    override def product[A, B](fa: Option[A], fb: Option[B]): Option[(A, B)] =
+      for {
+        a <- fa
+        b <- fb
+      } yield (a, b)
+  }
+}
