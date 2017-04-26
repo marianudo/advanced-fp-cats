c3e985f4c4ca014da658699fde61f15563300017
Mariano Navas <mariano.navas@clluc.com>
2017-04-26 17:53:52 +0200

    Rename point to pure to resemble cats notation

--- src/main/scala/ch04/Monad.scala
+++ src/main/scala/ch04/Monad.scala
@@ -6,10 +6,10 @@ import scala.language.higherKinds
 
 trait Monad[F[_]] extends Functor[F] {
 
-  def point[A](a: A): F[A]
+  def pure[A](a: A): F[A]
 
   def flatMap[A, B](fa: F[A])(f: A => F[B]): F[B]
 
   override def map[A, B](fa: F[A])(f: A => B): F[B] =
-    flatMap(fa)(a => point(f(a)))
+    flatMap(fa)(a => pure(f(a)))
 }
