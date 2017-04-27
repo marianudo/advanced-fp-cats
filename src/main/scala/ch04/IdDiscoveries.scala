551e4ec8e9b2a567e5f83064825a7829d4ab143b
Mariano Navas <mariano.navas@clluc.com>
2017-04-27 12:55:02 +0200

    Refactor Id implementation using book hints

--- src/main/scala/ch04/IdDiscoveries.scala
+++ src/main/scala/ch04/IdDiscoveries.scala
@@ -10,10 +10,9 @@ object Id extends Monad[Identity] {
   override def pure[A](a: A): Identity[A] = a
 
   override def flatMap[A, B](fa: Identity[A])(f: (A) => Identity[B]): Identity[B] = {
-    val a: A = fa
-    f(a)
+    f(fa)
   }
 
   override def map[A, B](fa: Identity[A])(f: A => B): Identity[B] =
-    flatMap(fa)(a => pure(f(a)))
+    f(fa)
 }
