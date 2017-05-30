23d05eb0402d4796a86f04eee2464bd454cdaa48
Mariano Navas <mariano.navas@clluc.com>
2017-05-30 17:03:55 +0200

    Replace previous Check impl with an ADT

--- src/main/scala/datavalidation/DataValidation.scala
+++ src/main/scala/datavalidation/DataValidation.scala
@@ -2,29 +2,28 @@ package datavalidation
 
 import cats.Semigroup
 import cats.syntax.semigroup._
+import cats.syntax.either._
 
 object DataValidation {
 //  type Check[E, A] = A => Either[E, A]
 
   sealed trait Check[E, A] {
 
-    def apply(a: A): Either[E, A]
-
-    def and(that: Check[E, A])(implicit sg: Semigroup[E]): Check[E, A]
-  }
-
-  case class CheckF[E, A](fx: A => Either[E, A]) extends Check[E, A] {
-
-    override def apply(a: A) = fx(a)
-
-    override def and(that: Check[E, A])(implicit sg: Semigroup[E]): Check[E, A] =
-      CheckF { a =>
-        (this(a), that(a)) match {
-          case (Left(e1), Left(e2)) => Left(e1 |+| e2)
-          case (Left(e), Right(_)) => Left(e)
-          case (Right(_), Left(e)) => Left(e)
-          case (Right(_), Right(_)) => Right(a)
+    def apply(a: A)(implicit sg: Semigroup[E]): Either[E, A] =
+      this match {
+        case Pure(fx) => fx(a)
+        case And(c1, c2) => (c1(a), c2(a)) match {
+          case (Left(e1), Left(e2)) => (e1 |+| e2).asLeft
+          case (Right(_), Left(e)) => e.asLeft
+          case (Left(e), Right(_)) => e.asLeft
+          case (Right(_), Right(_)) => a.asRight
         }
       }
+
+    def and(that: Check[E, A]): Check[E, A] = And(this, that)
   }
+
+  case class And[E, A](left: Check[E, A], right: Check[E, A]) extends Check[E, A]
+
+  case class Pure[E, A](fx: A => Either[E, A]) extends Check[E, A]
 }
