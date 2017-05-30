2c252c773f19e5559534a402030b732d7d002a9d
Mariano Navas <mariano.navas@clluc.com>
2017-05-30 17:09:40 +0200

    Prepare terrain to use Cartesian instead of Either

--- src/main/scala/datavalidation/DataValidation.scala
+++ src/main/scala/datavalidation/DataValidation.scala
@@ -7,23 +7,31 @@ import cats.syntax.either._
 object DataValidation {
 //  type Check[E, A] = A => Either[E, A]
 
-  sealed trait Check[E, A] {
+  object WithEither {
 
-    def apply(a: A)(implicit sg: Semigroup[E]): Either[E, A] =
-      this match {
-        case Pure(fx) => fx(a)
-        case And(c1, c2) => (c1(a), c2(a)) match {
-          case (Left(e1), Left(e2)) => (e1 |+| e2).asLeft
-          case (Right(_), Left(e)) => e.asLeft
-          case (Left(e), Right(_)) => e.asLeft
-          case (Right(_), Right(_)) => a.asRight
+    sealed trait Check[E, A] {
+
+      def apply(a: A)(implicit sg: Semigroup[E]): Either[E, A] =
+        this match {
+          case Pure(fx) => fx(a)
+          case And(c1, c2) => (c1(a), c2(a)) match {
+            case (Left(e1), Left(e2)) => (e1 |+| e2).asLeft
+            case (Right(_), Left(e)) => e.asLeft
+            case (Left(e), Right(_)) => e.asLeft
+            case (Right(_), Right(_)) => a.asRight
+          }
         }
-      }
 
-    def and(that: Check[E, A]): Check[E, A] = And(this, that)
+      def and(that: Check[E, A]): Check[E, A] = And(this, that)
+    }
+
+    case class And[E, A](left: Check[E, A], right: Check[E, A]) extends Check[E, A]
+
+    case class Pure[E, A](fx: A => Either[E, A]) extends Check[E, A]
+
   }
 
-  case class And[E, A](left: Check[E, A], right: Check[E, A]) extends Check[E, A]
-
-  case class Pure[E, A](fx: A => Either[E, A]) extends Check[E, A]
+  object WithValidated {
+    // TODO
+  }
 }
